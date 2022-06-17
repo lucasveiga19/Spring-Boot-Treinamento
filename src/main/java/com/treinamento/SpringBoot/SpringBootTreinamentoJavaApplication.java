64ccdb6f2be79ce25a2aa51b87397cebd923a098
Lucas Uchôa da Veiga <lucas.veiga@laveiga.com.br>
2022-06-17 18:23:11 -0300

    Inicialização

---
+++ src/main/java/com/treinamento/SpringBoot/SpringBootTreinamentoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTreinamentoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTreinamentoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinamento/SpringBoot/SpringBootTreinamentoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTreinamentoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
